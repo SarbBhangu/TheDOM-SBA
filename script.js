f2b79a7c12ce34a9a7eed3b3ce2a8a564bab3e98
SarbBhangu <Bhangu513@gmail.com>
2025-11-07 16:57:20 -0500

    finished the renderPosts function

--- script.js
+++ script.js
@@ -19,7 +19,34 @@ function loadPosts() {
         }
 }
 
+
 function savePosts() {
     localStorage.setItem('brainstorm-posts', JSON.stringify(posts));
 }
 
+
+function renderPosts() {
+    postsList.innerHTML = '';
+
+        if (posts.length === 0) {
+            const li = document.createElement('li');
+            li.textContent = 'Storm is coming';
+            li.style.opacity = '0.8';
+            postsList.appendChild(li);
+            return;
+        }       
+
+
+posts.forEach(function(post) {
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p')
+   
+    p.textContent = post.title;
+    
+    li.appendChild(h3)
+    li.appendChild(p);
+    postsList.appendChild(li);
+});
+}
+        
