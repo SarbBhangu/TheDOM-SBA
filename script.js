c740e1fd3f5b433156fb45a5e93d269ab57cc1d3
SarbBhangu <Bhangu513@gmail.com>
2025-11-07 16:13:47 -0500

    added posts array and local storage functions

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('newPostForm');
+const titleInput = document.getElementById('postTitle');
+const contentInput = document.getElementById('postContent');
+const titleError = document.getElementById('titleError');
+const contentError = document.getElementById('contentError');
+const postsList = document.getElementById('postsList');
+
+
+let posts = [];
+
+
+function loadPosts() {
+    const saved = localStorage.getItem('brainstorm-posts');
+
+        if (saved) {
+            posts = JSON.parse(saved);
+        } else {
+            posts = [];
+        }
+}
